12a92a6dd3bfb5eedb34cf6996e374e61efd3776
Samuel Pankov <zenleviantalforo@gmail.com>
2024-11-25 22:08:10 +0200

    Task 2. Students added

---
+++ PROGRAMING ADVANCED FOR QA/Unit 8. Objects and Classes/2. Students/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _2._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentsList = new List<Student>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] studentsData = input.Split().ToArray();
+                string firstName = studentsData[0];
+                string lastName = studentsData[1];
+                int age = int.Parse(studentsData[2]);
+                string homeTown = studentsData[3];
+
+                Student studentDetails = new Student(firstName, lastName, age, homeTown);
+                studentsList.Add(studentDetails);
+                input = Console.ReadLine();
+            }
+
+            string homeTownFilter = Console.ReadLine();
+
+            foreach (Student student in studentsList.Where(student => student.HomeTown == homeTownFilter))
+            {
+                Console.WriteLine($"{student.GivenName} {student.FamilyName} is {student.Age} years old.");
+            }
+
+        }
+
+        class Student
+        {
+            public Student(string givenName, string familyName, int age, string homeTown)
+            {
+                GivenName = givenName;
+                FamilyName = familyName;
+                Age = age;
+                HomeTown = homeTown;
+            }
+
+            public string GivenName { get; set; }
+            public string FamilyName { get; set; }
+            public int Age { get; set; }
+            public string HomeTown { get; set; }
+        }
+    }
+}
--- PROGRAMING ADVANCED FOR QA/Unit 8. Objects and Classes/Demo/Program.cs
+++ PROGRAMING ADVANCED FOR QA/Unit 8. Objects and Classes/Demo/Program.cs
@@ -4,16 +4,31 @@ smallDog.Name = "Ricky";
 smallDog.Breed = "Pincher";
 smallDog.Age = 1;
 
+Dog middleDog = new Dog("Bobo", "Corgi", 3);
+
 Console.WriteLine(smallDog.Bark());
+Console.WriteLine(middleDog.Bark() + " as well!");
+
 
 class Dog
 {
+    public Dog()
+    {
+        
+    }
+    public Dog(string name, string breed, int age)
+    {
+        Name = name;
+        Breed = breed;
+        Age = age;
+    }
+
     public string Name { get; set; }
     public string Breed { get; set; }
     public int Age {  get; set; }   
 
     public string Bark()
     {
-        return $"{this.Name}, the {Breed} barks!";
+        return $"{this.Name}, the {Breed} barks";
     }
 }
