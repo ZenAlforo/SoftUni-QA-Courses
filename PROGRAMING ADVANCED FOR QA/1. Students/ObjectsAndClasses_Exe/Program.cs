810d758593fc2bb0703cb44bca83dc0801249aa0
Samuel Pankov <zenleviantalforo@gmail.com>
2024-11-27 19:39:50 +0200

    Added Task 1. Students

---
+++ PROGRAMING ADVANCED FOR QA/1. Students/ObjectsAndClasses_Exe/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectsAndClasses_Exe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
