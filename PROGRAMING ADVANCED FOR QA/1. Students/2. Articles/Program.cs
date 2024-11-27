1c333dea80f25db77042a92835ba610a097c6dba
Samuel Pankov <zenleviantalforo@gmail.com>
2024-11-27 20:50:04 +0200

    Task 2 added to Unit

---
+++ PROGRAMING ADVANCED FOR QA/1. Students/2. Articles/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _2._Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] articleData = Console.ReadLine().Split(", ");
+            int rotations = int.Parse(Console.ReadLine());
+            (string title, string content, string author) = (articleData[0], articleData[1], articleData[2]);
+
+            Article currentArticle = new Article(title, content, author);
+
+            for (int i = 0; i < rotations; i++)
+            {
+                string[] newArticleData = Console.ReadLine().Split(": ");
+                string whatToDo = newArticleData[0];
+                if (whatToDo == "Edit")
+                {
+                    string newContent = newArticleData[1];
+                    currentArticle.Edit(newContent);
+                } else if (whatToDo == "ChangeAuthor")
+                {
+                    string newAuthor = newArticleData[1];
+                    currentArticle.ChangeAuthor(newAuthor);
+
+                }
+                else if (whatToDo == "Rename")
+                {
+                    string newTitle = newArticleData[1];
+                    currentArticle.Rename(newTitle);
+
+                }
+            }
+
+            Console.WriteLine($"{currentArticle.Title} - {currentArticle.Content}: {currentArticle.Author}");
+        }
+
+        class Article
+        {
+            public Article(string title, string content, string author)
+            {
+                Title = title;
+                Content = content;
+                Author = author;
+            }
+
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public string Author { get; set; }
+
+            public void Edit(string newContent)
+            {
+                this.Content = newContent;
+            }
+
+            public void ChangeAuthor(string newAuthor)
+            {
+                this.Author = newAuthor;
+            }
+
+            public void Rename(string newTitle)
+            {
+                this.Title = newTitle;
+            }
+        }
+    }
+}
--- PROGRAMING ADVANCED FOR QA/1. Students/ObjectsAndClasses_Exe/Program.cs
+++ PROGRAMING ADVANCED FOR QA/1. Students/ObjectsAndClasses_Exe/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Student> students = new() { };
+
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                string[] studentData = Console.ReadLine().Split();
+                string firstName = studentData[0];
+                string lastName = studentData[1];
+                double grade = double.Parse(studentData[2]);
+
+                Student currentStudent = new Student(firstName, lastName, grade);
+                students.Add(currentStudent);
+            }
+
+            foreach (Student student in students.OrderByDescending(student => student.Grade))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+            }
+
+        }
+
+        class Student
+        {
+            public Student(string firstName, string lastName, double grade)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Grade = grade;
+            }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public double Grade { get; set; }
         }
     }
 }
