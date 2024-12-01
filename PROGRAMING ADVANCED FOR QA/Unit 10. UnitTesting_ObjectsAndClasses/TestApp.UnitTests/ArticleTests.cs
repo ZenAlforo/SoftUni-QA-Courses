d5d6f58eac516748aa2ae543bd5d661368a983d7
Samuel Pankov <zenleviantalforo@gmail.com>
2024-12-01 16:06:41 +0200

    Task 6 - Articles added to lesson 10

--- PROGRAMING ADVANCED FOR QA/Unit 10. UnitTesting_ObjectsAndClasses/TestApp.UnitTests/ArticleTests.cs
+++ PROGRAMING ADVANCED FOR QA/Unit 10. UnitTesting_ObjectsAndClasses/TestApp.UnitTests/ArticleTests.cs
@@ -6,32 +6,63 @@ namespace TestApp.UnitTests;
 
 public class ArticleTests
 {
-    // TODO: write the setup method
+
+    private Article _currentArticle;
+
+    [SetUp] 
+    public void SetUp()
+    {
+        _currentArticle = new Article();
+    }
 
     // TODO: finish test
     [Test]
     public void Test_AddArticles_ReturnsArticleWithCorrectData()
     {
         // Arrange
+        string[] input = { "Nestle Chocolate Ivan", "NachevShop Nuts Sonia", "Banitsa Pastry Asia" };
 
         // Act
 
+        Article result = _currentArticle.AddArticles(input);
+
         // Assert
-        //Assert.That(result.ArticleList, Has.Count.EqualTo(3));
-        //Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article"));
-        //Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
-        //Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
+        Assert.That(result.ArticleList, Has.Count.EqualTo(3));
+        Assert.That(result.ArticleList[0].Title, Is.EqualTo("Nestle"));
+        Assert.That(result.ArticleList[1].Content, Is.EqualTo("Nuts"));
+        Assert.That(result.ArticleList[2].Author, Is.EqualTo("Asia"));
     }
 
     [Test]
     public void Test_GetArticleList_SortsArticlesByTitle()
     {
-        // TODO: finish test
+        // Arrange
+        string[] input = { "Nestle Chocolate Ivan", "NachevShop Nuts Sonia", "Banitsa Pastry Asia" };
+        Article result = _currentArticle.AddArticles(input);
+
+        string expected = $"Banitsa - Pastry: Asia{Environment.NewLine}" +
+                          $"NachevShop - Nuts: Sonia{Environment.NewLine}" +
+                          $"Nestle - Chocolate: Ivan";
+
+        // Act
+        string sortedArticleList = _currentArticle.GetArticleList(result, "title");
+
+        // Assert
+        Assert.That(sortedArticleList, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
     {
-        // TODO: finish test
+        // Arrange
+        string[] input = { "Nestle Chocolate Ivan", "NachevShop Nuts Sonia", "Banitsa Pastry Asia" };
+        string expected = string.Empty;
+
+        // Act
+        Article result = _currentArticle.AddArticles(input);
+        string sortedArticleList = _currentArticle.GetArticleList(result, "Animals");
+
+        // Assert
+        Assert.That(sortedArticleList, Is.EqualTo(expected));
     }
 }
