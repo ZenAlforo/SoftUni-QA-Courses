4e03512a09941264527a1073b3b2239d91186c97
Samuel Pankov <zenleviantalforo@gmail.com>
2024-12-01 17:10:13 +0200

    New lesson 11 added

---
+++ PROGRAMING ADVANCED FOR QA/Unit 11. ExceptionHandling/1. SquareRoot/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _1._SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    Console.WriteLine(Math.Sqrt(number));
+                }
+
+
+            }
+            catch (ArgumentException argEx)
+            {
+                Console.WriteLine("Invalid number.");
+            } finally 
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
