2fcc0db8041f1f30649f05cd01387e559c3ace9b
Samuel Pankov <zenleviantalforo@gmail.com>
2024-12-01 22:03:08 +0200

    Unit 12 opened, task 1-3 finished

---
+++ PROGRAMING ADVANCED FOR QA/Unit 12. UnitTestingExceptionHandling/TestApp.UnitTests/ExceptionTests.cs
@@ -0,0 +1,270 @@
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TestApp.UnitTests;
+
+public class ExceptionTests
+{
+    private Exceptions _exceptions = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._exceptions = new();
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_ValidString_ReturnsReversedString()
+    {
+        // Arrange
+        string input = "cat";
+        string expected = "tac";
+
+        // Act
+        string result = _exceptions.ArgumentNullReverse(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_NullString_ThrowsArgumentNullException()
+    {
+        // Arrange
+        string input = null;
+        string expected = "String cannot be null. (Parameter 's')";
+
+        // Act & Assert
+        Assert.That(() => this._exceptions.ArgumentNullReverse(input), Throws.ArgumentNullException);
+
+        try
+        {
+            this._exceptions.ArgumentNullReverse(input);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Assert.AreEqual(expected, ex.Message);
+        }
+    }
+
+    [Test]
+    public void Test_CalculateDiscount_ValidInput_ReturnsDiscountedPrice()
+    {
+        // Arrange
+        decimal price = 509;
+        decimal discount = 35;
+        decimal expected = 330.85m;
+
+        // Act
+        decimal result = _exceptions.ArgumentCalculateDiscount(price, discount);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_CalculateDiscount_NegativeDiscount_ThrowsArgumentException()
+    {
+        // Arrange
+        decimal price = 500;
+        decimal discount = -20;
+        string expected = "Discount must be between 0 and 100. (Parameter 'discount')";
+
+        // Act & Assert
+
+        ArgumentException exception = Assert.Throws<ArgumentException>(()=> this._exceptions.ArgumentCalculateDiscount(price, discount));
+        Assert.AreEqual(exception.Message, expected);
+
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_CalculateDiscount_DiscountOver100_ThrowsArgumentException()
+    {
+        // Arrange
+        decimal totalPrice = 100.0m;
+        decimal discount = 110.0m;
+
+        string expected = "Discount must be between 0 and 100. (Parameter 'discount')";
+
+        // Act & Assert
+
+        ArgumentException exception = Assert.Throws<ArgumentException>(() => this._exceptions.ArgumentCalculateDiscount(totalPrice, discount));
+        Assert.AreEqual(exception.Message, expected);
+
+    }
+
+    [Test]
+    public void Test_GetElement_ValidIndex_ReturnsElement()
+    {
+        // Arrange
+        int[] array = { 1, 2, 3 };
+        int index = 1;
+        int expected = 2;
+
+        // Act
+        int result = _exceptions.IndexOutOfRangeGetElement(array, index);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_GetElement_IndexLessThanZero_ThrowsIndexOutOfRangeException()
+    {
+        // Arrange
+        int[] array = { 1, 2, 3 };
+        int index = -2;
+        string expected = "Index is out of range.";
+
+        // Act & Assert
+        IndexOutOfRangeException error = Assert.Throws<IndexOutOfRangeException>(() => _exceptions.IndexOutOfRangeGetElement(array, index));
+        Assert.AreEqual(expected, error.Message);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_GetElement_IndexEqualToArrayLength_ThrowsIndexOutOfRangeException()
+    {
+        // Arrange
+        int[] array = { 10, 20, 30, 40, 50 };
+        int index = array.Length;
+
+        // Act & Assert
+        IndexOutOfRangeException error = Assert.Throws<IndexOutOfRangeException>(() => _exceptions.IndexOutOfRangeGetElement(array, index));
+        Assert.AreEqual("Index is out of range.", error.Message);
+    }
+
+    [Test]
+    public void Test_GetElement_IndexGreaterThanArrayLength_ThrowsIndexOutOfRangeException()
+    {
+        // Arrange
+        int[] array = { 10, 20, 30, 40, 50 };
+        int index = 7;
+
+        // Act & Assert
+        // checking the type of the exception thrown
+        Assert.That(() => _exceptions.IndexOutOfRangeGetElement(array, index), Throws.InstanceOf<IndexOutOfRangeException>());
+
+        try
+        {
+            _exceptions.IndexOutOfRangeGetElement(array, index);
+        } 
+        catch (IndexOutOfRangeException ex) 
+        {
+            Assert.AreEqual("Index is out of range.", ex.Message);
+        }
+        // other way to do it
+        //IndexOutOfRangeException error = Assert.Throws<IndexOutOfRangeException>(() => _exceptions.IndexOutOfRangeGetElement(array, index));
+        //Assert.AreEqual("Index is out of range.", error.Message);
+    }
+
+    [Test]
+    public void Test_PerformSecureOperation_UserLoggedIn_ReturnsUserLoggedInMessage()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_PerformSecureOperation_UserNotLoggedIn_ThrowsInvalidOperationException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_ParseInt_ValidInput_ReturnsParsedInteger()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_ParseInt_InvalidInput_ThrowsFormatException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_FindValueByKey_KeyExistsInDictionary_ReturnsValue()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_FindValueByKey_KeyDoesNotExistInDictionary_ThrowsKeyNotFoundException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_AddNumbers_NoOverflow_ReturnsSum()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_AddNumbers_PositiveOverflow_ThrowsOverflowException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_AddNumbers_NegativeOverflow_ThrowsOverflowException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_DivideNumbers_ValidDivision_ReturnsQuotient()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_DivideNumbers_DivideByZero_ThrowsDivideByZeroException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_SumCollectionElements_ValidCollectionAndIndex_ReturnsSum()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_SumCollectionElements_NullCollection_ThrowsArgumentNullException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_SumCollectionElements_IndexOutOfRange_ThrowsIndexOutOfRangeException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_GetElementAsNumber_ValidKey_ReturnsParsedNumber()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_GetElementAsNumber_KeyNotFound_ThrowsKeyNotFoundException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_GetElementAsNumber_InvalidFormat_ThrowsFormatException()
+    {
+        // TODO: finish test
+    }
+}
