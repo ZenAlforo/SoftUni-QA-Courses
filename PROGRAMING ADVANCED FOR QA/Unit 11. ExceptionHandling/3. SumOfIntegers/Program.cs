fd2ae6226b37f3d9bb57cf70593b6a1cd41366b3
Samuel Pankov <zenleviantalforo@gmail.com>
2024-12-01 20:49:17 +0200

    Task 3 added to unit 11

--- PROGRAMING ADVANCED FOR QA/Unit 11. ExceptionHandling/3. SumOfIntegers/Program.cs
+++ PROGRAMING ADVANCED FOR QA/Unit 11. ExceptionHandling/3. SumOfIntegers/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] elements = Console.ReadLine().Split();
+
+            int sum = 0;
+
+            foreach (string element in elements)
+            {
+                try
+                {
+                    int currentNumber = int.Parse(element);
+                    sum += currentNumber;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{element}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{element}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
         }
     }
 }
