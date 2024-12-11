1d1ef47c55fda9d4847662b264df1cd9ed52f18b
Samuel Pankov <zenleviantalforo@gmail.com>
2024-12-11 22:45:06 +0200

    Task 3 completed from Exam Prep 1

--- PROGRAMING ADVANCED FOR QA/Unit 17. Exam Prep 1/03-Product-Resources/ClassesResources/TestApp.Tests/ProductInventoryTests.cs
+++ PROGRAMING ADVANCED FOR QA/Unit 17. Exam Prep 1/03-Product-Resources/ClassesResources/TestApp.Tests/ProductInventoryTests.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-
+using System;
 using TestApp.Product;
 
 namespace TestApp.Tests;
@@ -18,30 +18,81 @@ public class ProductInventoryTests
     [Test]
     public void Test_AddProduct_ProductAddedToInventory()
     {
-        // TODO: finish the test
+        // Arrange
+        string productName = "apples";
+        double productPrice = 4.50;
+        int quantity = 4;
+
+        string expected = $"Product Inventory:{Environment.NewLine}" +
+            $"{productName} - Price: ${productPrice:F2} - Quantity: {quantity}";
+
+        // Act
+        _inventory.AddProduct(productName, productPrice, quantity);
+        string result = this._inventory.DisplayInventory();
+
+        // Assert
+        Assert.NotNull(this._inventory);
+        Assert.AreEqual(expected, result);
     }
 
     [Test]
     public void Test_DisplayInventory_NoProducts_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        // Arrange
+        string expected = $"Product Inventory:";
+
+        // Act
+        
+        string result = this._inventory.DisplayInventory();
+
+        // Assert
+        Assert.AreEqual(expected, result);
     }
 
     [Test]
     public void Test_DisplayInventory_WithProducts_ReturnsFormattedInventory()
     {
-        // TODO: finish the test
+        // Arrange
+        _inventory.AddProduct("cheese", 9.90, 1);
+        _inventory.AddProduct("milk", 2.90, 3);
+        _inventory.AddProduct("yellow cheese", 18.90, 1);
+
+        string expected = $"Product Inventory:{Environment.NewLine}" +
+            $"cheese - Price: $9.90 - Quantity: 1{Environment.NewLine}" +
+            $"milk - Price: $2.90 - Quantity: 3{Environment.NewLine}" +
+            $"yellow cheese - Price: $18.90 - Quantity: 1";
+
+        // Act
+        
+        string result = this._inventory.DisplayInventory();
+
+        // Assert
+        Assert.NotNull(this._inventory);
+        Assert.AreEqual(expected, result);
     }
 
     [Test]
     public void Test_CalculateTotalValue_NoProducts_ReturnsZero()
     {
-        // TODO: finish the test
+        // Arrange, Act & Assert
+        Assert.That(_inventory.CalculateTotalValue(), Is.EqualTo(0));
     }
 
     [Test]
     public void Test_CalculateTotalValue_WithProducts_ReturnsTotalValue()
     {
-        // TODO: finish the test
+        // Arrange
+        _inventory.AddProduct("cheese", 9.90, 1);
+        _inventory.AddProduct("milk", 2.90, 3);
+        _inventory.AddProduct("yellow cheese", 18.90, 1);
+
+        double expected = 37.5;
+
+        // Act
+
+        double result = this._inventory.CalculateTotalValue();
+
+        // Assert
+        Assert.AreEqual(expected, result);
     }
 }
