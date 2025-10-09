7097442ecea2bae2697cd83a4e2b4162c057e7c1
Samuel Pankov <zenleviantalforo@gmail.com>
2025-10-09 21:33:35 +0300

    Folders moved inside another one

---
+++ BACKEND_TECHNOLOGIES_BASICS/13.UnitTestingWithMocking/Get Greetings/GetGreeting/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GetGreeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetingProvider greetingProvider = new GreetingProvider(new FakeTimeProvider(new DateTime(2000, 02, 02, 23, 00, 00)));
+            string greeting = greetingProvider.GetGreeting();
+            Console.WriteLine(greeting);
+        }
+    }
+
+}
